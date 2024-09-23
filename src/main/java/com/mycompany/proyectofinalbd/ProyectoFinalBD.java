15a77887a456485b7135b7b5c621763c24db329c
Germanrca <germanrenecaal@hotmail.com>
2024-09-23 10:30:09 -0600

    Carga inicial

---
+++ src/main/java/com/mycompany/proyectofinalbd/ProyectoFinalBD.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalbd;
+
+/**
+ *
+ * @author germa
+ */
+public class ProyectoFinalBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
