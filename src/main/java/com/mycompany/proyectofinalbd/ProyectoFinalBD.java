cafd325215baa74acebec8d2efa9ee362bf519e8
Germanrca <germanrenecaal@hotmail.com>
2024-09-23 12:29:38 -0600

    Cambio realizado: nombres

--- src/main/java/com/mycompany/proyectofinalbd/ProyectoFinalBD.java
+++ src/main/java/com/mycompany/proyectofinalbd/ProyectoFinalBD.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.proyectofinalbd;
 
 /**
- *
- * @author germa
- */
+*@author
+*José Eduardo Cristobal López 2690-21-5692
+* Christian Javier Elias Loarca 2690 - 22 - 10544
+* German René Catalán Alvarado 2690-21-8393
+*/
 public class ProyectoFinalBD {
 
     public static void main(String[] args) {
